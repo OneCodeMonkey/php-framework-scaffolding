df1a7c2ff11d34ee1596769b45a8c652b2c6a4f9
OneCodeMonkey <1460018362@qq.com>
2020-03-30 21:47:53 +0800

    modify

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Container\Container;
+use Illuminate\Database;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController
 {
 	public function index()
 	{
-		$student = Student::first();
-		$data = $student->getAttributes();
+		$test = User::first();
+		$data = $test->getAttributes();
 
 		$app = Container::getInstance();
 		$factory = $app->make('view');
--- config/database.php
+++ config/database.php
@@ -1,11 +1,19 @@
 <?php
 
 return [
-	'driver'		=> 'mysql',
-	'host'			=> 'localhost',
-	'database'		=> 'php_framework_scaffolding',
-	'username'		=> 'homestead',
-	'password'		=> 'secret',
+//	'driver'		=> 'mysql',
+//	'host'			=> 'localhost',
+//	'database'		=> 'php_framework_scaffolding',
+//	'username'		=> 'homestead',
+//	'password'		=> 'secret',
+//	'charset'		=> 'utf8mb4',
+//	'collation'		=> 'utf8mb4_general_ci',
+//	'prefix'		=> ''
+    'driver'		=> 'mysql',
+	'host'			=> '192.168.0.252',
+	'database'		=> 'test',
+	'username'		=> 'user_dev',
+	'password'		=> 'thefair@2019',
 	'charset'		=> 'utf8mb4',
 	'collation'		=> 'utf8mb4_general_ci',
 	'prefix'		=> ''
